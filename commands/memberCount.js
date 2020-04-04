f702375d86668bb0cb6bc856fc561c64ad0017ef
nathandunning <dunningn@gmail.com>
2020-04-05 10:34:12 +1200

    Added other outlines

--- commands/memberCount.js
+++ commands/memberCount.js
@@ -1,7 +1,8 @@
 module.exports = {
   name: 'memberCount',
   description: 'Returns a count of the number of members in the server',
-  execute(message, args) {
+  guideOnly: true,
+  execute(message) {
     message.channel.send(
       `Total Members in server: ${message.guild.memberCount}`
     );
