a4b281080997a0fa9128ec9fae09baee5b8f0062
Nathan <nathan.dunning@nzrb.co.nz>
2020-04-12 16:23:24 +1200

    Finished help command

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports = {
 
     if (!args.length) {
       data.push("Here's a list of all my commands:");
-      data.push(commands.map(command => command.name).join(', '));
+      data.push(commands.map((command) => command.name).join(', '));
       data.push(
         `\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`
       );
@@ -23,7 +23,7 @@ module.exports = {
           if (message.channel.type === 'dm') return;
           message.reply("I've sent you a DM with all my commands!");
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(
             `Could not send help DM to ${message.author.tag}.\n`,
             error
@@ -33,7 +33,21 @@ module.exports = {
           );
         });
     } else {
-      //TODO:
+      const name = args[0].toLowerCase();
+      const command =
+        commands.get(name) ||
+        commands.find((c) => c.aliases && c.aliases.includes(name));
+
+      if (!command) {
+        return message.reply("That's not a valid command!");
+      }
+
+      if (command.aliases) data.push(`Aliases: ${command.aliases.join(', ')}`);
+      if (command.description) data.push(`Description: ${command.description}`);
+      if (command.usage)
+        data.push(`Usage: ${prefix}${command.name} ${command.usage}`);
+
+      message.channel.send(data, { split: true });
     }
-  }
+  },
 };
