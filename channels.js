bde5d40fed19c50905b48a89939334c943012bff
Nathan <nathan.dunning@nzrb.co.nz>
2020-04-13 14:45:23 +1200

    Working on music

---
+++ channels.js
@@ -0,0 +1,6 @@
+module.exports = {
+  general: '244095507033489408',
+  commands: '259228078490910721',
+  testgeneral: '698760455191003176',
+  testcommands: '694764131147972689',
+};
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
+const { testgeneral, testcommands } = require('./channels.js');
 const fs = require('fs');
 
 // Command handling
 client.commands = new Discord.Collection();
 const commandFiles = fs
   .readdirSync('./commands')
-  .filter(file => file.endsWith('.js'));
-console.log(commandFiles);
+  .filter((file) => file.endsWith('.js'));
 
-// Dynamic require + Setting commands
+// Dynamic require + Setting/loading commands
 for (let file of commandFiles) {
   let command = require(`./commands/${file}`);
-  console.log(command);
   client.commands.set(command.name, command);
 }
 
@@ -26,41 +25,68 @@ client.login(token);
 
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}!`);
+  // const channel = client.channels.cache.get(testcommands);
+  // if (channel) channel.send('ready');
+  // else console.error('Unable to find channel with id:', testingtext);
 });
 
 // Message listener
-client.on('message', message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-  // Split args and command
-  const args = message.content.slice(prefix.length).split(/ +/);
-  const commandName = args.shift().toLowerCase();
-  console.log(commandName, args);
-
-  // Check valid command
-  if (!client.commands.has(commandName)) return;
-  const command = client.commands.get(commandName);
-
-  // Check guide only command
-  if (command.guildOnly && message.channel.type !== 'text') {
-    return message.reply(
-      "You sneaky pan, I can't execute that command inside DMs!"
-    );
-  }
-
-  // Check command args
-  if (command.args && !args.length) {
-    let reply = `${message.author} you sala pan, I can't execute that with no arguments \n`;
-    if (command.usage) {
-      reply += `The proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+client.on('message', (message) => {
+  // Check for prefixed message or bot message
+  if (
+    message.content.startsWith(prefix) ||
+    message.content.startsWith('!') ||
+    message.author.bot
+  ) {
+    // Check message in correct channgel
+    if (message.channel.id === testgeneral) {
+      message
+        .delete()
+        .then(
+          console.log(
+            `Deleted message from ${message.author.username}(${message.author.id})`
+          )
+        )
+        .catch((err) => {
+          console.error(er);
+        });
+      message.author.send(
+        'Please use the command channel for anything bot related'
+      );
     }
-    return message.channel.send(reply);
-  }
+    // Execute command block
+    else {
+      // Split args and command
+      const args = message.content.slice(prefix.length).split(/ +/);
+      const commandName = args.shift().toLowerCase();
 
-  // Execute
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply(`There was an error executing: ${command}`);
+      // Check valid command
+      if (!client.commands.has(commandName)) return;
+      const command = client.commands.get(commandName);
+
+      // Check guild only command
+      if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply(
+          "You sneaky pan, I can't execute that command inside DMs!"
+        );
+      }
+
+      // Check command args
+      if (command.args && !args.length) {
+        let reply = `${message.author} you sala pan, I can't execute that with no arguments \n`;
+        if (command.usage) {
+          reply += `The proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+        return message.channel.send(reply);
+      }
+
+      // Execute
+      try {
+        command.execute(message, args);
+      } catch (error) {
+        console.error(error);
+        message.reply(`There was an error executing: ${command}`);
+      }
+    }
   }
 });
