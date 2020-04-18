6281c632d62996a0ac68913a36c3afe3c19761a3
nathandunning <dunningn@gmail.com>
2020-04-18 17:30:03 +1200

    Adding new config, connected to DB

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix } = require('./config.json');
 const textChannels = require('./config/channels.js');
-const { welcome } = require('./config/config.js');
-const { ErelaClient, Utils } = require('erela.js');
+const configHandler = require('./config/configHandler.js');
 const fs = require('fs');
 require('dotenv').config();
 
@@ -24,38 +23,27 @@ const load = (dirs) => {
 };
 ['miscellaneous', 'moderation', 'music'].forEach((f) => load(f));
 
-// Nodes for Erela.js
-const nodes = [{ host: 'localhost', port: 2333, password: 'youshallnotpass' }];
-
 // Initiate client
-client.once('ready', () => {
-  console.log('Ready!');
+client.once('ready', async () => {
+  // Connect to DB
+  await configHandler.startDBConnection().then(res => console.log("Successfuly connected to DB")).catch(err => console.log(err));
+  await configHandler.initialiseErela(client).then(res => console.log(res)).catch(err => { console.log(err) })
 
-  // // Initialise Erela client
-  // client.music = new ErelaClient(client, nodes);
-  // // Event Listener
-  // client.music.on('nodeConnect', () => console.log('New node connected'));
-  // client.music.on('nodeError', (error) =>
-  //   console.log(`Node error: ${error.message}`)
-  // );
-  // client.music.on('trackStart', ({ textChannel }, { title, duration }) =>
-  //   textChannel.send(
-  //     `Now playing: **${title}** \`${Utils.formatTime(duration, true)}\``
-  //   )
-  // );
-
+  // Set levels
   client.levels = new Map()
     .set('none', 0.0)
     .set('low', 0.1)
     .set('medium', 0.15)
     .set('high', 0.25);
+  configHandler.welcome(client);
+
+  console.log('Ready!');
 });
 
 client.login(process.env.DISCORD_CLIENT_TOKEN);
 
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}!`);
-  welcome(client);
 });
 
 // Message listener
@@ -121,3 +109,12 @@ client.on('message', (message) => {
     }
   }
 });
+
+// Message reaction listener
+client.on('messageReactionAdd', (messageReaction, user) => {
+  configHandler.messageReactionAdd(client, messageReaction, user)
+})
+
+client.on('messageReactionRemove', (messageReaction, user) => {
+  configHandler.messageReactionRemove(client, messageReaction, user)
+})
