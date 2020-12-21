9acdded4f6bd1b459f400e8636a4297d300c6dbc
Nathan <nathan.dunning@nzrb.co.nz>
2020-12-22 11:55:41 +1300

    Market support

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const { prefix } = require('./config.json');
 const client = new Discord.Client({
   partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
