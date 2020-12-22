b95e6b76f47947050ca033b559dcd16302311ee7
Nathan <nathan.dunning@nzrb.co.nz>
2020-12-22 19:08:23 +1300

    Removing Erela and music commands

--- config/configHandler.js
+++ config/configHandler.js
@@ -1,4 +1,3 @@
-const { ErelaClient, Utils } = require('erela.js');
 const { Client } = require('pg');
 const textChannels = require('./channels.js');
 const emojis = require('./emojis.js');
@@ -20,30 +19,6 @@ async function startDBConnection() {
 async function dbQuery(query) {
   return db.query(query);
 }
-
-//FIXME: Remove this - save formatting
-// async function initialiseErela(client) {
-//   // Nodes for Erela.js
-//   const nodes = [
-//     { host: 'localhost', port: 2333, password: 'youshallnotpass' },
-//   ];
-
-//   return new Promise((resolve, reject) => {
-//     // Initialise Erela client
-//     client.music = new ErelaClient(client, nodes);
-
-//     // Add Event Listener
-//     client.music.on('nodeConnect', () => resolve('New node connected'));
-//     client.music.on('nodeError', (error) =>
-//       reject(`Node error: ${error.message}`)
-//     );
-//     client.music.on('trackStart', ({ textChannel }, { title, duration }) =>
-//       textChannel.send(
-//         `Now playing: **${title}** \`${Utils.formatTime(duration, true)}\``
-//       )
-//     );
-//   });
-// }
 
 async function welcome(client) {
   const rulesChannel = client.channels.cache.get(`${textChannels.rules.id}`);
